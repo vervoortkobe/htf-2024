e5cabd349868e7463204be91efc7a30ec15139ee
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2024-11-20 12:09:15 +0100

    Update seeding

--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -20,6 +20,18 @@ async function seedDatabase() {
         description:
           "A rigorous assessment of your reflexes and response time. In this exercise, your objective is to swiftly click on blue dots that appear on the screen at unpredictable intervals. The quicker and more accurately you respond, the better your performance will be. This challenge is designed to measure your reaction time and focus, offering valuable insights into your ability to respond under pressure. Are you prepared to demonstrate your precision and speed?",
       },
+      {
+        id: 2,
+        name: "Pong Challenge",
+        description:
+          "Get as much points as you can by hitting the ball with your paddle. Use your mouse to move your paddle up and down. The longer you survive, the higher your score will be. Good luck!",
+      },
+      {
+        id: 3,
+        name: "Space Shooter Challenge",
+        description:
+          "Watch out for the asteroids while navigating through space. Use your mouse to move your spaceship left and right and avoid the obstacles. Is an asteroid coming too close? You can shoot the asteroids to destroy them and eliminating the danger. The longer you survive, the higher your score will be. Good luck!",
+      },
     ])
     .execute();
 
